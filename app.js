14d07a4738a927fe716dfa9c04acfb284ae25a2f
Austin-Derrick <aderrickson@colum.edu>
2021-06-20 21:57:22 -0500

    Require mongoose in file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
