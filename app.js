fcb5694543b2f9dd05fe68b9de86ffb32987a92b
Austin-Derrick <aderrickson@colum.edu>
2021-06-20 22:06:34 -0500

    Connected to mongo database

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const app = express();
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+})
+
+
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-
 app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/makecampground', (req, res) => {
     res.render('home');
 })
 
