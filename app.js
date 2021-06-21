a7f06838551f9829801af9d021234499efd12824
Austin-Derrick <aderrickson@colum.edu>
2021-06-20 23:12:37 -0500

    Added route to editing page

--- app.js
+++ app.js
@@ -38,13 +38,21 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', async (req, res)=>{
-    res.send(req.body);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
     const id = req.params.id;
     const campground =  await Campground.findById(id);
     res.render('campgrounds/show', {campground});
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const id = req.params.id;
+    const campground =  await Campground.findById(id);
+    res.render('campgrounds/edit', {campground});
 });
 
 app.listen(3000, () => {
