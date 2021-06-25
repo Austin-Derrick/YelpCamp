84ead48aff2d5fc9707ff4b09baf4445783b813f
Austin-Derrick <aderrickson@colum.edu>
2021-06-25 10:16:03 -0500

    removed unused campground routes and added logic for storing user in session

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 
-const campgrounds = require('./routes/campgrounds.js');
-const reviews = require('./routes/reviews.js');
+const userRoutes = require('./routes/users.js')
+const campgroundRoutes = require('./routes/campgrounds.js');
+const reviewRoutes = require('./routes/reviews.js');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -44,16 +48,31 @@ const sessionConfig = {
 };
 
 app.use(session(sessionConfig));
-app.use(flash())
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({email: 'coltttt@gmail.com', username: 'colt'});
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser);
+})
 
 app.get('/', (req, res) => {
     res.render('home');
